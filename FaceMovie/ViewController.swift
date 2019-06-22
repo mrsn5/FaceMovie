7ed7d466bb0689d1120588661259a2caeaf1bbd5
San Nguyen <mr.sn5@icloud.com>
2019-06-22 21:34:33 +0300

    Initial Commit

---
+++ FaceMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceMovie
+//
+//  Created by San Byn Nguyen on 6/22/19.
+//  Copyright © 2019 San Byn Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
